7f95dbe3d2a61df3b6474a1297078f902447d2ab
Scoresby <andresdaniel@gmail.com>
2021-09-28 22:12:04 +0200

    Delete comments

--- src/main/java/io/decker/config/ProjectSecurityConfig.java
+++ src/main/java/io/decker/config/ProjectSecurityConfig.java
@@ -2,18 +2,10 @@ package io.decker.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -31,40 +23,10 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin().and()
                 .httpBasic();
-
-//        http .authorizeRequests() .anyRequest().denyAll() .and()
-//                .formLogin().and().httpBasic();
-
-//        http .authorizeRequests() .anyRequest().permitAll() .and()
-//                .formLogin().and().httpBasic();
-
-
     }
-
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication().withUser("admin").password("12345").authorities("admin").and()
-//                .withUser("user").password("12345").authorities("read").and()
-//                .passwordEncoder(NoOpPasswordEncoder.getInstance());
-//    }
-
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
-//        UserDetails user = User.withUsername("admin").password("12345").authorities("admin").build();
-//        UserDetails user1 = User.withUsername("user").password("12345").authorities("read").build();
-//        userDetailsManager.createUser(user);
-//        userDetailsManager.createUser(user1);
-//        auth.userDetailsService(userDetailsManager);
-//    }
-
-//    @Bean
-//    UserDetailsService userDetailsService(DataSource dataSource) {
-//        return new JdbcUserDetailsManager(dataSource);
-//    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
